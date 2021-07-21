60aedd2bcc18764c0aec80b7e2201b617dbb72b1
frankcasanova <frankcasanova.info@gmail.com>
2021-07-21 16:56:38 +0200

    user registration

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls.base import reverse, resolve
+from .forms import CustomUserCreationForm
+from .views import SignUpPageView
+
 
 # Create your tests here.
 
@@ -33,3 +37,25 @@ class CustomUserTest(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTest(TestCase):
+
+    def setUp(self) -> None:
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'malandro!')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddleware')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignUpPageView.as_view().__name__)
