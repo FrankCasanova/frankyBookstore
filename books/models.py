1293cfe1c460989caafbb82c0056ac86c4d0c839
frankcasanova <frankcasanova.info@gmail.com>
2021-07-28 18:10:40 +0200

    books app added

--- books/models.py
+++ books/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from uuid import uuid4
 
 # Create your models here.
 
 
 class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default= uuid4,
+        editable=False
+    )
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -13,5 +19,5 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("book_detail", kwargs={"pk": self.pk})
+       return reverse('book_detail', args=[str(self.id)])
     
