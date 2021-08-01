98f26cafe05cbd15f647030b77eebd022f79268a
Frank Casanova <frankcasanova.info@gmail.com>
2021-08-01 22:48:59 +0200

    search funtionality implemented

--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from .models import Book
 # Create your views here.
+
 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
@@ -9,11 +11,22 @@ class BookListView(LoginRequiredMixin, ListView):
     template_name = 'books/book_list.html'
     login_url = 'account_login'
 
-    
 
-class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin, DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
-    login_url = 'account_login'    
+    login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):  #
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
